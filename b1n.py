41d24652863d4e3913aa3de899e88665a60c274c
Sombranegra666 <71173038+Sombranegra666@users.noreply.github.com>
2021-01-02 11:28:14 -0300

    Update b1n.py

--- b1n.py
+++ b1n.py
@@ -7,27 +7,28 @@ $$ |      $$ \$$$$ |$$ |  $$ |  \___$$\       $$  __$$\   $$ |  $$ \$$$$ |
 $$ |  $$\ $$ |\$$$ |$$ |  $$ |$$\   $$ |      $$ |  $$ |  $$ |  $$ |\$$$ |
 \$$$$$$  |\$$$$$$  /$$$$$$$  |\$$$$$$  |      $$$$$$$  |$$$$$$\ $$ | \$$ |
  \______/  \______/ \_______/  \______/       \_______/ \______|\__|  \__|
-[By Dreifus] [v3.0]
- """)
+[By Dreifus] [v3.0] [Leia o README.md]""")
 
-print("[1]: Converter Numero Normal em Binário"), print("[2]: Converter bite Em Byte"),print("[3]: Converter Byte Em bite"), print()
+print("[1]: Converter Numero Normal em Binário")
+print("[2]: Converter bite Em Byte")
+print("[3]: Converter Byte Em bite")
 pc1=int(input("> "))
+
 if(pc1==1):
-    print("[!!] Obs: Ignore o 0b [!!]")
-    print("[|] set One Number"), print()
-    n1=int(input("> "))
-    n2=bin(n1)
-    print("bin> {}".format(n2))
+   print()
+   print("[•] set One Number"), print()
+   n1=int(input("> "))
+   n2=bin(n1)
+   print("bin> {}".format(n2))
 if(pc1==2):
-    print()
-    print("[•] Coloque a Quantidade de bits"), print()
-    n3=int(input("> "))
-    n4=n3//8
-    print("[+]:Isso gera mais ou menos {} Bytes".format(n4))
+   print()
+   print("[•] Coloque a Quantidade de bits"), print()
+   n3=int(input("> "))
+   n4=n3/8
+   print("[+]:Isso gera mais ou menos {} Bytes".format(n4))
 if(pc1==3):
-    print()
-    print("[•] Coloque a Quantidade de Bytes"), print()
-    n5=int(input("> "))
-    n6=n5*8
-    print("[+]: Isso gera {} bits".format(n6))
-    
+   print()
+   print("[•] Coloque a Quantidade de Bytes"), print()
+   n5=int(input("> "))
+   n6=n5*8
+   print("[+]: Isso é mais ou menos {} bits".format(n6))
